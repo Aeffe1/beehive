a0e989a968d7e835affb25f80543036a4942c765
Aeffe1 <120492841+Aeffe1@users.noreply.github.com>
2025-09-26 14:34:52 -0400

    Add files via upload

---
+++ get_log_lines.py
@@ -0,0 +1,175 @@
+import os
+import sys
+import re
+from datetime import datetime
+
+#This works but needs some refactoring
+#python -m pytest get_log_lines_test.py -vv
+
+#  FOR RUNNING PYTEST: comment the sys.argv lines and uncomment the COV and LIB lines. When running it for production, uncomment the print line at the end.
+class Constants:
+    pass
+        #COV_DATE = '20250519'
+        #LIB_CODE = 'e8w'
+        #COV_DATE = sys.argv[1]
+        #LIB_CODE = sys.argv[2]
+    
+
+class FileLocation:
+    
+    def __init__(self, cov_date=None, lib_code=None, path=r"C:\Users\megrust\Desktop\Workstuff\OCLC Datasync"):
+        self.cov_date = '20250519'
+        self.lib_code = 'e8w'
+        #change these to what they are "subject"
+        self.path = path
+
+    @property
+    def pathname(self):
+        return(
+            f'{self.path}\\Xrefs-{self.cov_date}-{self.lib_code}'
+            ) 
+            
+    @property
+    def input_date(self):
+        return(
+            f'{self.cov_date}'
+            )
+            
+            
+    # libcode is output - do not delete
+    @property
+    def libcode(self):
+        return(
+            f'{self.lib_code}'
+            )
+    
+
+    @property
+    def mrc_file_name(self):
+        #Look for the relevant files in this directory:
+        return self.get_file_name('.mrc')
+
+    
+    @property
+    def keyfilename(self):
+        return self.get_file_name('keys.txt')
+
+
+    @property
+    def xreffilename(self):
+        for (root,dirs,files) in os.walk(f'{self.pathname}'):
+            for file_name in files:
+                if "BibCrossRefReport" in file_name:
+                    return(
+                        f'{file_name}'
+                        )                    
+                                       
+    @property
+    def keycount(self):
+        for (root,dirs,files) in os.walk(f'{self.pathname}'): 
+            for file_name in files:
+                if "keys.txt" in file_name:
+                    with open(root+"\\"+file_name, "r") as k:
+                        for line in k:
+                            if "records kept" in line:
+                                kcount = re.search(r"\d+", line)
+
+                                return(
+                                    int(kcount.group())
+                                    )
+  
+    @property
+    def xrefcount(self):
+        for (root,dirs,files) in os.walk(f'{self.pathname}'): 
+            for file_name in files:
+                if "BibCrossRefReport" in file_name:
+                    with open(root+"\\"+file_name, "r") as f:
+                        line_count = sum(1 for line in f)
+                        return(
+                            line_count
+                            )
+
+    @property
+    def reportname(self):
+        for (root,dirs,files) in os.walk(f'{self.pathname}'):
+            for file_name in files:
+                if "_report.txt" in file_name:
+                    return(
+                        f'{file_name}'
+                        )
+
+                                    
+    @property
+    def report_data(self):
+        s = []
+        for (root,dirs,files) in os.walk(f'{self.pathname}'):         
+            for file_name in files:
+                if "_report.txt" in file_name:
+                    with open(root+"\\"+file_name, "r") as d:
+                        for line in d:
+                            if "updates" not in line:
+                                digits = re.search(r"\d+", line)
+                                dataline = str(digits.group())
+                                s.append(dataline)
+                        return ('\t'.join(s))
+               
+               
+    @property
+    def key_create_date(self):
+        c_time = os.path.getctime(f'{self.pathname}\\{self.keyfilename}')
+        dt_create = datetime.fromtimestamp(c_time).strftime('%Y%m%d')
+        return(
+            dt_create
+            )
+            
+    @property
+    def xref_create_date(self):
+        c_time = os.path.getctime(f'{self.pathname}\\{self.xreffilename}')
+        dt_create = datetime.fromtimestamp(c_time).strftime('%Y%m%d')
+        return(
+            dt_create
+            )
+    
+    @property
+    def report_create_date(self):
+        c_time = os.path.getctime(f'{self.pathname}\\{self.reportname}')
+        dt_create = datetime.fromtimestamp(c_time).strftime('%Y%m%d')
+        return(
+            dt_create
+            )  
+    
+    @property
+    def number_not_returned(self):
+        not_returned = int(self.keycount) - int(self.xrefcount)
+        return(
+            not_returned
+            )
+
+
+    def get_file_name(self, fragment):
+        for (root,dirs,files) in os.walk(f'{self.pathname}'): 
+            for file_name in files:
+                if fragment in file_name:
+                    return(
+                        f'{file_name}'
+                     )
+    
+    
+    def reportresult(self):
+        s = []
+        s.append(f'{self.mrc_file_name}')
+        s.append(f'{self.libcode}')
+        s.append('bib')
+        s.append(f'{self.keycount}')
+        s.append(f'{self.input_date}')
+        s.append(f'{self.key_create_date}')
+        s.append(f'{self.xref_create_date}')
+        s.append(f'{self.xrefcount}')
+        s.append(f'{self.number_not_returned}')
+        s.append(f'{self.report_create_date}')
+        s.append(f'{self.report_data}')
+        stats = '\t'.join(filter(None, s))
+        return(stats)
+       
+    
+#print(FileLocation().reportresult())
---
+++ get_log_lines_test.py
@@ -0,0 +1,114 @@
+import os
+import sys
+import pytest
+from datetime import datetime
+from get_log_lines import FileLocation
+
+@pytest.fixture
+def subject():
+    return FileLocation(cov_date='20250519', lib_code='e8w')
+    
+def test_path(subject):
+    expected = (
+        r'C:\Users\megrust\Desktop\Workstuff\OCLC Datasync\Xrefs-20250519-e8w'
+        )
+    assert subject.pathname == expected
+    
+def get_c_date():
+    expected = (
+        '20250519'
+        )
+    assert FileLocation().getcdate == expected
+    
+#@pytest.mark.skip    
+def test_filename():
+    expected = (
+        '1012672.e8w.bib_20250519_040511.mrc'
+        )
+    assert FileLocation().mrc_file_name == expected
+    
+    
+def test_get_date():
+    expected = (
+        '20250519'
+        )
+    assert FileLocation().input_date == expected
+        
+        
+def test_get_libname():
+    expected = (
+        'e8w'
+        )
+    assert FileLocation().libcode == expected
+    
+    
+def test_get_the_gd_file():
+    expected = (
+        'e8w-bib-20250519-keys.txt'
+        )
+    assert FileLocation().keyfilename == expected
+    
+    
+def test_xreffilename():
+    expected = (
+        'E8W-E8W.1012672.IN.BIB.D20250519.T081003757.1012672.e8w.bib_20250519_040511.mrc.BibCrossRefReport.txt'
+        )
+    assert FileLocation().xreffilename == expected    
+    
+      
+def test_key_count():
+    expected = (
+        379
+        )
+    assert FileLocation().keycount == expected
+    
+    
+def test_xref_count():
+    expected = (
+        379
+        )
+    assert FileLocation().xrefcount == expected
+    
+    
+def test_result_data():
+    expected = (
+        '1012672.e8w.bib_20250519_040511.mrc\te8w\tbib\t379\t20250519\t20250519\t20250519\t379\t0\t20250519\t293\t86\t0\t0'
+        )
+    assert FileLocation().reportresult() == expected
+    
+    
+def test_key_create_date():
+    expected = (
+        '20250519'
+        )
+    assert FileLocation().key_create_date == expected
+    
+    
+def test_xref_create_date():
+    expected = (
+        '20250519'
+        )
+    assert FileLocation().xref_create_date == expected
+    
+    
+def test_report_create_date():
+    expected = (
+        '20250519'
+        )
+    assert FileLocation().report_create_date == expected
+    
+
+def test_report_data():
+    expected = (
+        '293\t86\t0\t0'
+        )
+    assert FileLocation().report_data == expected
+    
+   
+def test_number_not_returned():
+    expected = (
+        0
+        )
+    assert FileLocation().number_not_returned == expected
+
+ 
